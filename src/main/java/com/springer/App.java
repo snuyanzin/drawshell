970d67788e10b6a124172ebd5dbe81e11bf02a93
Sergey Nuyanzin <snuyanzin@gmail.com>
2018-09-15 14:34:05 +0300

    Initial maven project Added .gitignore

---
+++ src/main/java/com/springer/App.java
@@ -0,0 +1,13 @@
+package com.springer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
